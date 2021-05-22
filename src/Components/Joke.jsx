be1232e653dac318f6196012bb5fb46d577852d6
Tereza Vecková <te.veckova@gmail.com>
2021-05-22 19:25:16 +0200

    rozdělaná 15.otázka

---
+++ src/Components/Joke.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './style.css';
+
+export const Joke = (props) => {
+  console.log(props);
+  const [up, setUp] = useState(0);
+  const [down, setDown] = useState(0);
+
+  return (
+    <>
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
+            <img
+              className="user-avatar"
+              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
+            />
+            <p className="user-name">Neroxx</p>
+          </div>
+          <p className="joke__text">
+            The secret service isn't allowed to yell "Get down!" anymore when
+            the president is about to be attacked. Now they have to yell
+            "Donald, duck!"
+          </p>
+        </div>
+        <div className="joke__likes">
+          <button
+            id="btn-up"
+            className="btn-like btn-like--up"
+            onClick={() => setUp(up + 1)}
+          ></button>
+          <span id="likes-up" className="likes-count likes-count--up">
+            {up}
+          </span>
+          <button
+            id="btn-down"
+            className="btn-like btn-like--down"
+            onClick={() => setDown(down + 1)}
+          ></button>
+          <span id="likes-down" className="likes-count likes-count--down">
+            {down}
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,47 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
+import { Joke } from './Components/Joke';
+import { jokes } from './joke.js';
 
-const App = () => {
-  const [up, setUp] = useState(0);
-  const [down, setDown] = useState(0);
+const App = (props) => {
   return (
     <>
       <div className="container">
-        <div className="joke">
-          <div className="joke__body">
-            <div className="joke__user">
-              <img
-                className="user-avatar"
-                src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-              />
-              <p className="user-name">Neroxx</p>
-            </div>
-            <p className="joke__text">
-              The secret service isn't allowed to yell "Get down!" anymore when
-              the president is about to be attacked. Now they have to yell
-              "Donald, duck!"
-            </p>
-          </div>
-          <div className="joke__likes">
-            <button
-              id="btn-up"
-              className="btn-like btn-like--up"
-              onClick={() => setUp(up + 1)}
-            ></button>
-            <span id="likes-up" className="likes-count likes-count--up">
-              {up}
-            </span>
-            <button
-              id="btn-down"
-              className="btn-like btn-like--down"
-              onClick={() => setDown(down + 1)}
-            ></button>
-            <span id="likes-down" className="likes-count likes-count--down">
-              {down}
-            </span>
-          </div>
-        </div>
+        <Joke seznamVtipu={jokes} />
       </div>
     </>
   );
